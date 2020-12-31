4b115c91ef4b667e9e056162313cfea583957057
Marcel Wagner <marcel_wagner_@arcor.de>
2020-12-31 12:23:43 +0100

    Working usage of camera input.

--- Programmieren/Programme/Aufgabe3/PictureEditor.py
+++ Programmieren/Programme/Aufgabe3/PictureEditor.py
@@ -15,10 +15,40 @@ import numpy as np
 from tkinter import filedialog as fd
 from PIL import ImageTk, ImageFilter, Image
 
+import cv2
 
 # Default size of the image
 pixels_x = 500
 pixels_y = 500
+
+# Class to use the webcam
+class MyVideoCapture:
+   def __init__(self, video_source=0):
+      # Open the video source
+      self.vid = cv2.VideoCapture(video_source)
+      self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, pixels_x)
+      self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, pixels_y)
+      if not self.vid.isOpened():
+         raise ValueError("Unable to open video source", video_source)
+      # Get video source width and height
+      self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+      self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+   # Release the video source when the object is destroyed
+   def __del__(self):
+      if self.vid.isOpened():
+         self.vid.release()
+
+   def get_frame(self):
+      if self.vid.isOpened():
+         ret, frame = self.vid.read()
+         if ret:
+            # Return a boolean success flag and the current frame converted to BGR
+            return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+         else:
+            return (ret, None)
+      else:
+         return (ret, None)
 
 # Class used to provide all the functions to manipulate the image
 class ManipulateImage:
@@ -30,10 +60,16 @@ class ManipulateImage:
       self.Sy = np.array([[-1, -2, -1],[0, 0, 0],[1, 2, 1]]) #Sobel Matrix in y-Richtung
       self.result = np.zeros((3,3))
 
-   # Function to open a image
+   # Function to open a new image
    def openImage(self, path_in):
       self.imgOrig = Image.open(path_in)
       self.img = self.imgOrig
+      self.cols, self.rows = self.imgOrig.size
+
+   # Function to update the image
+   def updateImage(self, img):
+      self.imgOrig = img
+      self.img = img
       self.cols, self.rows = self.imgOrig.size
 
    # Function to reset the manipulated image to the default
@@ -97,12 +133,20 @@ class App:
    def __init__(self):
       self.root = tk.Tk()
       self.root.title("PictureEditor")
+
+      self.delay = 20
+      self.camEnable = tk.IntVar()
+      self.sobelFilterEnable = tk.IntVar()
+
       self.manIp = ManipulateImage()
+      self.vid = MyVideoCapture()
+
       self.filedialog = tk.Button(self.root, text="Open Image", command= self.openDialog)
       self.filedialog.grid(row=0, column=1)
-      img = ImageTk.PhotoImage(self.manIp.img)
-      self.panel = tk.Label(self.root, image = img)
+
+      self.panel = tk.Canvas(self.root, width = self.vid.width, height = self.vid.height)
       self.panel.grid(row=0, column=0, rowspan=8)
+
       self.reset = tk.Button(self.root, text="Reset Image", command=self.resetImage)
       self.reset.grid(row=1,column=1)
       self.brightness = tk.Scale(self.root, from_=-100, to=100, label="Brightness", orient=tk.HORIZONTAL)
@@ -120,9 +164,39 @@ class App:
       self.fastEdgeFilter.grid(row=6, column=1)
       self.edgeFilter = tk.Button(self.root, text="Filter edges (slow (really slow))", command=self.edgeFilterFunc)
       self.edgeFilter.grid(row=7, column=1)
+
+      self.webCamEnable = tk.Checkbutton(self.root, text="Enable Webcam stream", variable=self.camEnable, command=self.deactivateButtons)
+      self.webCamEnable.grid(row=0, column=2)
+      self.sobelEnable = tk.Checkbutton(self.root, text="Endable sobel edge filter on webcam stream", variable=self.sobelFilterEnable, state=tk.DISABLED)
+      self.sobelEnable.grid(row=1, column=2)
+
+      self.update()
+
       self.root.mainloop()
 
    # Callback functions
+   def deactivateButtons(self):
+      if(self.camEnable.get()):
+         self.filedialog.config(state=tk.DISABLED)
+         self.reset.config(state=tk.DISABLED)
+         self.brightness.config(state=tk.DISABLED)
+         self.gamma.config(state=tk.DISABLED)
+         self.binarize.config(state=tk.DISABLED)
+         self.removeNoise.config(state=tk.DISABLED)
+         self.fastEdgeFilter.config(state=tk.DISABLED)
+         self.edgeFilter.config(state=tk.DISABLED)
+         self.sobelEnable.config(state=tk.NORMAL)
+      else:
+         self.filedialog.config(state=tk.NORMAL)
+         self.reset.config(state=tk.NORMAL)
+         self.brightness.config(state=tk.NORMAL)
+         self.gamma.config(state=tk.NORMAL)
+         self.binarize.config(state=tk.NORMAL)
+         self.removeNoise.config(state=tk.NORMAL)
+         self.fastEdgeFilter.config(state=tk.NORMAL)
+         self.edgeFilter.config(state=tk.NORMAL)
+         self.sobelEnable.config(state=tk.DISABLED)
+
    def resetScales(self):
       self.brightness.set('0')
       self.gamma.set('0')
@@ -154,21 +228,35 @@ class App:
    
    def resetImage(self):
       self.manIp.resetImage()
+      self.resetScales()
       self.updatePicture()
 
    def openDialog(self):
       path = fd.askopenfilename(title = "Select image", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png"), ("all files", "*.*")))
       if len(path) > 0:
          self.manIp.openImage(path)
-         img = ImageTk.PhotoImage(self.manIp.img)
-         self.panel.configure(image=img)
-         self.panel.image=img
+         self.updatePicture()
       self.resetScales()
 
+   def update(self):
+      if(self.camEnable.get()):
+         # Get a frame from the video source
+         ret, frame = self.vid.get_frame()
+
+         if ret:
+            self.manIp.updateImage(Image.fromarray(frame))
+            if(self.sobelFilterEnable.get()):
+               self.manIp.fastSobelEdgeFilter()
+            self.image = ImageTk.PhotoImage(image = self.manIp.img)
+            self.panel.config(height=self.manIp.rows, width=self.manIp.cols)
+            self.panel.create_image(0, 0, image = self.image, anchor = tk.NW)
+
+      self.root.after(self.delay, self.update)
+
    def updatePicture(self):
-      img = ImageTk.PhotoImage(self.manIp.img)
-      self.panel.configure(image=img)
-      self.panel.image=img
+      self.image = ImageTk.PhotoImage(self.manIp.img)
+      self.panel.config(height=self.manIp.rows, width=self.manIp.cols)
+      self.panel.create_image(0, 0, image = self.image, anchor = tk.NW)
 
 # Mainloop
 if __name__ == "__main__":
